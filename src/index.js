c6216bc6ed96d08b72ba71027cefaa86166cdb2d
Anthony Okpako <melowsworld@yahoo.co.uk>
2020-05-16 20:24:54 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createBrowserHistory} from 'history';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const hist = createBrowserHistory();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
